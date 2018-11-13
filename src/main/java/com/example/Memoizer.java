a620dd0b861a5cc2a388cfad108cc9fa1927dcc9
josephh <josephh@rocketmail.com>
2018-11-13 11:12:35 +0000

    updates improved memoizer uses future tasks

--- src/main/java/com/example/Memoizer.java
+++ src/main/java/com/example/Memoizer.java
@@ -3,12 +3,12 @@ package com.example;
 import net.jcip.annotations.GuardedBy;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
 
 public class Memoizer<A, V> implements Computable {
 
     @GuardedBy("this")
-    private final Map<A, V> cache = new ConcurrentHashMap<>();
+    private final Map<A, Future<V>> cache = new ConcurrentHashMap<>();
     private final Computable<A, V> c;
 
     public Memoizer(Computable<A, V> c) {
@@ -17,14 +17,41 @@ public class Memoizer<A, V> implements Computable {
 
     @Override
     public V compute(Object arg) throws InterruptedException {
-        V result = cache.get(arg);
+        Future<V> f = cache.get(arg);
         A a = (A) arg;
-        if (result == null) {
-            System.out.println(a +  " factorial not cached...computing");
-            result = c.compute(a);
-            cache.put(a, result);
+        if(f == null) {
+//            Callable<V> eval = new Callable<V>() {
+//                @Override
+//                public V call() throws Exception {
+//                    System.out.println(a +  " factorial not cached...computing");
+//                    return c.compute(a);
+//                }
+//            }
+//            FutureTask<V> ft = new FutureTask<>(eval);
+            /** The above verbose code - defining a Callable object and passing it
+             * to the FutureTask constructor, can be rewritten more concisely with
+             * a lambda directly in the FutureTask constructor.
+             */
+            FutureTask<V> ft = new FutureTask<>(() -> {
+                System.out.println(a +  " factorial not cached...computing");
+                return c.compute(a);
+            });
+            f = ft;
+            cache.put(a, ft);  // the future (rather than a known result) goes in the cache
+            ft.run();
+            f = ft;
+            cache.put(a, ft);
+            ft.run();
         }
-        return result;
+        try{
+            return f.get();
+        } catch (ExecutionException e) {
+            throw launderException(e.getCause());
+        }
+    }
+
+    private InterruptedException launderException(Throwable cause) {
+        throw new RuntimeException("Error: " + cause);
     }
 
 }
