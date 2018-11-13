29523d2d41b84e96c106fd37b339ee5c4977e2eb
josephh <josephh@rocketmail.com>
2018-11-13 08:56:55 +0000

    update to run multiple threads through memoizer

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,19 +2,41 @@ package com.example;
 
 public class Main {
 
-    public static void main(String args[]){
+    static Memoizer1 m1 = null ;
+
+    public static void main(String args[]) {
+        long t = System.currentTimeMillis();
         String s = "6";
         ExpensiveFunction ef = new ExpensiveFunction();
-        Memoizer1 m1 = new Memoizer1(ef);
-        try {
-            long t = System.currentTimeMillis();
-            System.out.println("??????? Factorial of " + s + " = " + m1.compute(s));
-            System.out.println("First elapsed time = " + (System.currentTimeMillis() - t));
-            t = System.currentTimeMillis();
-            System.out.println("??????? Second factorial calculation " + s + " = " + m1.compute(s));
-            System.out.println("(cached) elapsed time = " + (System.currentTimeMillis() - t));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        m1 = new Memoizer1(ef);
+        ClientThreads ct = new ClientThreads("3");
+        ClientThreads ct1 = new ClientThreads("4");
+        ClientThreads ct2 = new ClientThreads("3");
+        ct.run();
+        ct1.run();
+        ct2.run();
+        System.out.println(">>> Total running time : " + (System.currentTimeMillis() - t));
+    }
+
+    private static class ClientThreads implements Runnable {
+        private final String s;
+
+        public ClientThreads(String s) {
+            this.s = s;
+        }
+
+        @Override
+        public void run() {
+            try {
+                long t = System.currentTimeMillis();
+                System.out.println("??????? Factorial of " + s + " = " + m1.compute(s));
+                System.out.println("::: compute(...) elapsed running time : " + (System.currentTimeMillis() - t));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
+
+
+
